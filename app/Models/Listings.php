8f793a540264940016fb9e6d6e4cf64598a63a4b
Farhan Khan <farhan.k2005@gmail.com>
2022-09-27 15:11:34 -0400

    used php artisan to create listing model

--- app/Models/Listings.php
+++ app/Models/Listings.php
@@ -1,30 +1,11 @@
-<?php 
+<?php
+
 namespace App\Models;
 
-class Listings{
-    public static function getAll(){
-        return [
-            [
-                'id' => '1',
-                'title' => 'Lisiting One',
-                'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, excepturi explicabo. Quas sapiente blanditiis dolore nam porro quia aperiam, amet suscipit saepe, veritatis rerum ullam labore ratione recusandae, quisquam sed.'
-            ],
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-            [
-                'id' => '2',
-                'title' => 'Lisiting Two',
-                'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, excepturi explicabo. Quas sapiente blanditiis dolore nam porro quia aperiam, amet suscipit saepe, veritatis rerum ullam labore ratione recusandae, quisquam sed.'
-            ]
-        ];
-    }
-
-    public static function findID($id){
-        $listings = self::getAll();
-
-        foreach($listings as $listing){
-            if($listing['id'] == $id){
-                return $listing;
-            }
-        }
-    }
+class Listings extends Model
+{
+    use HasFactory;
 }
