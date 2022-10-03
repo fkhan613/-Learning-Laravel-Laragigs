d21a44808b28cd458dfacdc8f675ef3eb30c671c
Farhan Khan <farhan.k2005@gmail.com>
2022-10-02 21:21:21 -0400

    created relationship between user and listing

--- database/migrations/2022_09_27_185604_create_listings_table.php
+++ database/migrations/2022_09_27_185604_create_listings_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('listings', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade') ->nullable();
             $table -> string('title') ->nullable();
             $table -> string('tags')->nullable();
             $table -> string('company_name')->nullable();
